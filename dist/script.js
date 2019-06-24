84879bd6d3df70028a14ee838dfa93e366bbbbc2
C Cooper <cfcooper@uci.edu>
2019-06-23 17:43:32 -0700

    Add bg image, html, css, and js from cactus landing page

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+function onClickMenu() {
+  document.getElementById("menu").classList.toggle("change");
+  //this toggle changes class list every time menu is clicked: it adds class="change" to things that have class="menu"
+
+  document.getElementById("navbar").classList.toggle("change");
+}
